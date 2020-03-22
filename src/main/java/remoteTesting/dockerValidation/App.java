483fa462cb7b16c3fde3f3590dc0dcaec0e224dc
unknown <neeraj.bakhtani@gmail.com>
2020-03-22 22:18:45 +0800

    Runs docker wait till All Hub and Nodes are configured executes test cases and then stops the Docker

---
+++ src/main/java/remoteTesting/dockerValidation/App.java
@@ -0,0 +1,10 @@
+package remoteTesting.dockerValidation;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
